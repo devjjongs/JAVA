93c99541328125c2a756e34cd15fb7f32b56d89e
devjjongs <whdtjr5689@gmail.com>
2021-07-08 10:10:36 +0900

    add practice source

---
+++ javaProject/src/prj/member/MemberControllerImpl.java
@@ -0,0 +1,35 @@
+package prj.member;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+
+public class MemberControllerImpl  implements MemberController{
+	public MemberDAO dao;
+	
+	@Override
+	public ArrayList<MemberVo> listMember() {
+		ArrayList<MemberVo> lst = new ArrayList<MemberVo>();
+		dao = new MemberDAOImpl();
+		lst = dao.listMember();
+		return lst;
+	}
+
+	@Override
+	public void insertMember(MemberVo memberVo) {
+		dao = new MemberDAOImpl();
+		dao.insertMember(memberVo);
+		
+	}
+
+	@Override
+	public void updateMember(MemberVo memberVo) {
+		
+	}
+
+	@Override
+	public void deleteMember(MemberVo memberVo) {
+		
+	}
+
+}
